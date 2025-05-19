5f5102c801c1300a86d742a95fb3444f07b2b2db
Lucas Alves <lucasalves.57@hotmail.com>
2025-05-19 20:06:36 -0300

    feat: implementando novos recursos em restaurante repository

--- src/main/java/org/estudos/algafoods/restaurante/RestauranteRepository.java
+++ src/main/java/org/estudos/algafoods/restaurante/RestauranteRepository.java
@@ -3,5 +3,10 @@ package org.estudos.algafoods.restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long> { }
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+    List<Restaurante> findByNomeContainingIgnoreCase(String nome);
+}
