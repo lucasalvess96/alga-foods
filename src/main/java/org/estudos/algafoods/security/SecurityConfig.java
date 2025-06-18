45fc4ad29eb683053858f26f5bf84267ad4c909b
Lucas Alves <lucasalves.57@hotmail.com>
2025-06-18 00:14:20 -0300

    refactor: ajustando securityConfig para permitir customOAuth2SuccessHandler e jwtAuthenticationFilter

--- src/main/java/org/estudos/algafoods/security/SecurityConfig.java
+++ src/main/java/org/estudos/algafoods/security/SecurityConfig.java
@@ -5,10 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomOAuth2SuccessHandler customOAuth2SuccessHandler;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    public SecurityConfig(CustomOAuth2SuccessHandler customOAuth2SuccessHandler, JwtAuthenticationFilter jwtAuthenticationFilter) {
+        this.customOAuth2SuccessHandler = customOAuth2SuccessHandler;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -17,8 +27,8 @@ public class SecurityConfig {
                         .requestMatchers("/api/public").permitAll()
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(customizer -> {
-                });
+                .oauth2Login(customizer -> customizer.successHandler(customOAuth2SuccessHandler))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
