e386e4ad2ac33b89625e20a0ba27ac7197329ca9
Lucas Alves <lucasalves.57@hotmail.com>
2025-09-23 08:46:00 -0300

    feat: método filtro de pesquisa paginado RestauranteRepository

--- src/main/java/org/estudos/algafoods/restaurante/RestauranteRepository.java
+++ src/main/java/org/estudos/algafoods/restaurante/RestauranteRepository.java
@@ -1,5 +1,7 @@
 package org.estudos.algafoods.restaurante;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.List;
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
 
     List<Restaurante> findByNomeContainingIgnoreCase(String nome);
+
+    Page<Restaurante> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
